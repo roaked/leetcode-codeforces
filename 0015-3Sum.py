33d1aa1b9498ef33bcd43f38d8cbdcb9148cff6c
roaked <ossunarola@gmail.com>
2023-12-05 20:30:45 +0100

    thressum

--- 0015-3Sum.py
+++ 0015-3Sum.py
@@ -4,17 +4,38 @@
 # Notice that the solution set must not contain duplicate triplets.
 
 class Solution(object):
-    def threeSum(self, nums): # int[nums] type list       
-        num_idx = {} 
-        final = []
-        counter = []
-        while (len(counter) <= 3):
-            for idx, num in enumerate(nums): # type list ---> Pos. 0 / 1
-                if nums[idx]:
-                    #return [num_idx[num],idx]
-                    return final[nums[idx]]
-                else:
-                    num_idx[num]
+    def threeSum(self, nums): # int[nums]  LIST
+        nums.sort() # [-4, -1, -1, 0, 1, 2 ]
+        number = len(nums) # 
+        result = []
+        for i in range(number - 2):
+        #duplicates
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+        left, right = i + 1, n - 1  # Pointers for the other two numbers
+
+        while left < right:
+            total = nums[i] + nums[left] + nums[right]
+
+            if total < 0:
+                left += 1
+            elif total > 0:
+                right -= 1
+            else:
+                result.append([nums[i], nums[left], nums[right]])
+
+                # Skip duplicate values for the second number in the triplet
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                # Skip duplicate values for the third number in the triplet
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1
+
+                left += 1
+                right -= 1
+
+        return result
 
 nums = [-1,0,1,2,-1,-4]
 print(Solution().threeSum(nums))
