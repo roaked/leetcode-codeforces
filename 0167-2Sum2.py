e2109298ad459c5b3d6e0ea16127f74118de7745
roaked <ossunarola@gmail.com>
2023-12-07 14:07:20 +0100

    dictionary

--- 0167-2Sum2.py
+++ 0167-2Sum2.py
@@ -7,24 +7,44 @@
 
 #Dictionary
 class Solution(object):
-    def twoSum(self, nums, target):
+    def twoSumD(self, nums, target):
         num_idx = {}
         for idx, num in enumerate(nums):
             diff = target - num
             if diff in num_idx:
-                #print('\nInput: {0}\nTarget: {1}\nIndex 1: {2}\nIndex 2: {3}'.format(nums, target, num_idx[diff]+1, idx+1))
+                print('\nInput: {0}\nTarget: {1}\nIndex 1: {2}\nIndex 2: {3}'.format(nums, target, num_idx[diff]+1, idx+1))
                 return [num_idx[diff]+1, idx+1]
             else:
                 num_idx[num] = idx
 
+#Binary Search (already sorted list)
+    def twoSumBS(self, nums, target):
+        investigatedSoFar = []
+        for i in range(len(nums)):
+            if not nums[i] in investigatedSoFar:
+                investigatedSoFar.append(nums[i])
+                l, r = i + 1, len(nums) - 1
+                tmp = target - nums[i]
+                while l <= r:
+                    mid = l + (r-l) // 2
+                    if nums[mid] == tmp:
+                        return([i + 1, mid + 1])
+                    elif nums[mid] < tmp:
+                        l = mid + 1
+                    else:
+                        r = mid - 1
+
 nums = [2,7,11,15]
 target = 9
-Solution().twoSum(nums, target)
+#Solution().twoSumD(nums, target)
+print(Solution().twoSumBS(nums, target))
 
 nums = [2,3,4]
 target = 6
-Solution().twoSum(nums, target)
+#Solution().twoSumD(nums, target)
+print(Solution().twoSumBS(nums, target))
 
 nums = [-1,0]
 target = -1
-Solution().twoSum(nums, target)
+#Solution().twoSumD(nums, target)
+print(Solution().twoSumBS(nums, target))
