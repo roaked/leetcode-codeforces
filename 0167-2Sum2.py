0fd785195c5d6fe1b64f19b3486e4b2523e54e55
roaked <ossunarola@gmail.com>
2023-12-07 13:29:59 +0100

    twosumtwo

---
+++ 0167-2Sum2.py
@@ -0,0 +1,24 @@
+# Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that 
+# they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 < numbers.length.
+# Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+# The tests are generated such that there is exactly one solution. You may not use the same element twice.
+# Your solution must use only constant extra space.
+
+class Solution(object):
+    def twoSumTwo(self, numbers, target):
+
+
+
+        return
+    
+nums = [2,7,11,15]
+target = 9
+print(Solution().twoSumTwo(nums, target))
+
+nums = [2,3,4]
+target = 6
+print(Solution().twoSumTwo(nums, target))
+
+nums = [-1,0]
+target = -1
+print(Solution().twoSumTwo(nums, target))
