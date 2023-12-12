0c82bae711da99d650371af8dd77cb78c5df9a57
roaked <ossunarola@gmail.com>
2023-12-12 09:54:30 +0100

    1287

--- 1287-findSpecialInteger.py
+++ 1287-findSpecialInteger.py
@@ -1,19 +1,18 @@
 #Given an integer array sorted in non-decreasing order, there is exactly one integer in the array that occurs more than 25% of the time,
 # return that integer.
-
 class Solution(object):
     def findSpecialInteger(self, arr):
-        threshold = int(0.25 * len(arr))
-        counter = 0
+        threshold = len(arr) // 4
+        counter = 1
+        solution = arr[0]
         for i in range(len(arr) - 1):
             if arr[i] == arr[i + 1]:
                 counter += 1
                 if counter > threshold:
                     return arr[i]
             else:
-                counter = 0
-        return None
-
+                counter = 1
+        return solution
 
 arr = [1,1]
 print(Solution().findSpecialInteger(arr))
