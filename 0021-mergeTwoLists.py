fad3f2014f03de737e69535c258a40238dc64cd3
roaked <ossunarola@gmail.com>
2023-12-12 14:06:24 +0100

    0021

--- 0021-mergeTwoLists.py
+++ 0021-mergeTwoLists.py
@@ -3,12 +3,7 @@ You are given the heads of two sorted linked lists list1 and list2.
 Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
 Return the head of the merged linked list.
 """
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
+# Definition of LL
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
