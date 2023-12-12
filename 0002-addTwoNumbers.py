27849c5d74dade2c00b67b74fa39a3f862aa15c7
roaked <ossunarola@gmail.com>
2023-12-12 20:02:42 +0100

    add

--- 0002-addTwoNumbers.py
+++ 0002-addTwoNumbers.py
@@ -12,29 +12,30 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode):
-        dummy = cur = ListNode()
+        dummy = cur = ListNode() # creating and pointer
         print(ListNode())
         remain = 0
         while l1 or l2 or remain: # not empty
-            v1 = l1.val if l2 is not None else 0
-            v2 = l2.val if l1 is not None else 0
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
             sum = v1 + v2 + remain
-            value = sum % 10  # 9/10 = 1 
-            remain = sum // 10 # 9/10 = 0 (floor)
+            value = sum % 10  # 9 mod 10 = 1 
+            remain = sum // 10 # 9 floor 10 = 0
 
             newNode = ListNode(value)
             cur.next = newNode
             cur = cur.next
             
-            l1 = l1.next if l1 is not None else None
-            l2 = l2.next if l2 is not None else None
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
 
         res = dummy.next
         dummy.next = None
         return res
         
-l1 = ListNode(1, ListNode(4, ListNode(3)))
+l1 = ListNode(2, ListNode(4, ListNode(3)))
 l2 = ListNode(5, ListNode(6, ListNode(4)))
+# 342 + 465 = 807
 
 sol = Solution().addTwoNumbers(l1, l2)
 while sol:
