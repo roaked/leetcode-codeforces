7a1e66df680d544709fb607ff5d27e17b501d512
roaked <ossunarola@gmail.com>
2023-12-12 10:23:17 +0100

    0001

--- 0001-twoSum.py
+++ 0001-twoSum.py
@@ -2,30 +2,8 @@
 
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-# You can return the answer in any order.
-
-# Example 1:
-
-# Input: nums = [2,7,11,15], target = 9
-# Output: [0,1]
-# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-# Example 2:
-
-# Input: nums = [3,2,4], target = 6
-# Output: [1,2]
-# Example 3:
-
-# Input: nums = [3,3], target = 6
-# Output: [0,1]
-
-#num_idx   Ex1  t = 9
-# 0 -> 2
-# 1 -> 7
-# 2 -> 11
-# 3 -> 15
-
 class Solution:
-    def twoSum(self, nums, target):
+    def twoSum(self, nums: list, target: int):
         num_idx = {}
         for idx, num in enumerate(nums):  #0,2
             diff = target - num  #diff = 9 - 2 = 7
