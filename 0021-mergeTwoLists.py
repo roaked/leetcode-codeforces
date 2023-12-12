1c948e1dc34ff2bb8764931c706d82138cddf328
roaked <ossunarola@gmail.com>
2023-12-12 13:57:06 +0100

    0021

--- 0021-mergeTwoLists.py
+++ 0021-mergeTwoLists.py
@@ -9,21 +9,50 @@ Return the head of the merged linked list.
 #         self.val = val
 #         self.next = next
 
-from typing import Optional, ListNode
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]: # None values
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode):
+        dummy = ListNode()
+        current = dummy
+        
+        while list1 is not None and list2 is not None:
+            if list1.val <= list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            
+            current = current.next
 
-        return None
+        current.next = list1 if list1 else list2
+        
+        return dummy.next
 
-list1 = [1,2,4]
-list2 = [1,3,4]
-print(Solution().mergeTwoLists(list1, list2))
+list1 = ListNode(1, ListNode(2, ListNode(4)))
+list2 = ListNode(1, ListNode(3, ListNode(4)))
 
-list1 = []
-list2 = []
-print(Solution().mergeTwoLists(list1, list2))
+sol = Solution().mergeTwoLists(list1, list2)
+while sol:
+    print(sol.val, end=" ")
+    sol = sol.next
 
-list1 = []
-list2 = [0]
-print(Solution().mergeTwoLists(list1, list2))
+"""
+list1 = ListNode()
+list2 = ListNode()
+sol = Solution().mergeTwoLists(list1, list2)
+while sol:
+    print(sol.val, end=" ")
+    sol = sol.next
+
+list1 = ListNode()
+list2 = ListNode(1)
+sol = Solution().mergeTwoLists(list1, list2)
+while sol:
+    print(sol.val, end=" ")
+    sol = sol.next
+"""
