98f0a5ba29015ff0e37e8022cc31e91643738475
roaked <ossunarola@gmail.com>
2023-12-13 17:57:23 +0100

    reverse sort > pop/negative/zero > dfs/backtracking for combinations

--- 0039-combinationSum.py
+++ 0039-combinationSum.py
@@ -6,39 +6,33 @@ frequency of at least one of the chosen numbers is different.
 
 The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input."""
 
-class Solution:
-    def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
+class Solution(object):
+    def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]: #Better than 99% Run time + 91% Memory
         sol = []
-        candidates.sort()
-        for candidate in candidates[::-1]:
+        candidates.sort(reverse=True) # bigger first
+        for i in range(len(candidates)-1,-1,-1):
+            candidate = candidates[i]
             if candidate > target:
-                candidates.pop()
+                candidates.pop(i)  
             elif candidate == target:
                 sol.append([candidate])
-                candidates.pop()
+                candidates.pop(i) 
                 break
+            elif candidate <= 0:
+                candidates.pop(i)  #zero or negative
             else:
                 continue
-        #print(sol) [7]
-        #print(candidates) [2, 3, 6]
-        size = len(candidates) #3
 
+        SIZE = len(candidates)
         def dfs(combination, current_sum, idx):
-            if current_sum > target: 
+            if current_sum > target or current_sum < 0: #technically < 0 already secured previously
                 return
             if current_sum == target: 
                 sol.append(combination)
-            for i in range(idx, size): 
+            for i in range(idx, SIZE): 
                 dfs(combination + [candidates[i]], current_sum + candidates[i], i)
-        
         dfs([],0,0)
         return sol
-    
-        # for i in range(size): #0,1,2
-        #     print(i)
-        #     while candidates[i+1]:
-        #         if candidates[i] + candidates[i+1] > target: continue
-        #         elif candidates[i] + candidates[i+1] == target: sol.append(candidates[i])
 
 candidates = [2,3,6,7]
 target = 7
@@ -50,4 +44,8 @@ print(Solution().combinationSum(candidates,target))
 
 candidates = [2]
 target = 1
+print(Solution().combinationSum(candidates,target))
+
+candidates = [7,0,-1,1,2,3,4]
+target = 7
 print(Solution().combinationSum(candidates,target))
