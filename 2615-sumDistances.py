9d9c1733dd98c3cc359d8d3c300aebb3cae5d29d
roaked <ossunarola@gmail.com>
2023-12-14 20:36:08 +0100

    up

--- 2615-sumDistances.py
+++ 2615-sumDistances.py
@@ -1,5 +1,6 @@
 """You are given a 0-indexed integer array nums. There exists an array arr of length nums.length,
- where arr[i] is the sum of |i - j| over all j such that nums[j] == nums[i] and j != i. If there is no such j, set arr[i] to be 0.
+ where arr[i] is the sum of |i - j| over all j such that nums[j] == nums[i] and j != i. 
+ If there is no such j, set arr[i] to be 0.
 
 Return the array arr.
 """
