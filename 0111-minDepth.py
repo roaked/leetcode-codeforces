6b2861e6c3aa6ec755841961a5b282e258db87e2
roaked <ossunarola@gmail.com>
2023-12-15 13:48:51 +0100

    ifcond - finding minimum depth // binary trees

---
+++ 0111-minDepth.py
@@ -0,0 +1,40 @@
+"""Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children."""
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        leftDepth = self.minDepth(root.left)
+        rightDepth = self.minDepth(root.right)
+        
+        if not root.left and not root.right:
+            return 1
+
+        if not root.left:
+            return 1 + rightDepth
+        
+        if not root.right:
+            return 1 + leftDepth        
+        
+        return min(leftDepth, rightDepth) + 1
+    
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+
+sol = Solution()
+res = (sol.minDepth(root))
+print(res)
