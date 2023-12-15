4873964df87d9a5977490e33d80afa3db01f84d1
roaked <ossunarola@gmail.com>
2023-12-15 13:19:23 +0100

    assertions

---
+++ 0026-removeDuplicates.py
@@ -0,0 +1,19 @@
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        if not nums:
+            return 0
+        
+        index = 1 
+        
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[index] = nums[i]
+                index += 1
+        
+        return index
+
+sol = Solution()
+print(sol.removeDuplicates(nums=[1, 1, 2])) 
+print(sol.removeDuplicates(nums=[0, 0, 1, 1, 1, 2, 2, 3, 3, 4])) 
+print(sol.removeDuplicates(nums=[0, 1, 2, 2, 2, 2, 2, 3, 4, 4, 4])) 
+print(sol.removeDuplicates(nums=[1, 1, 1, 1, 1]))  
