51c9725f40cf6e035779939ad9c4d2be0ef84a91
roaked <ossunarola@gmail.com>
2023-12-19 19:22:52 +0100

    just loops

--- 0661.imageSmoother.py
+++ 0661.imageSmoother.py
@@ -5,18 +5,26 @@ If one or more of the surrounding cells of a cell is not present,
  we do not consider it in the average (i.e., the average of the four cells in the red smoother)."""
 
 class Solution:
-    def imageSmoother(self, img: list[list[int]]) -> list[list[int]]:
-        r, c = len(img), len(img[0]) # rows, columns
-        n = [0] * r
-        m = [0] * r
+    def imageSmoother(matrix):
+        if not matrix:
+            return []
 
-        for row in range(r):
-            for column in range(c):
-                while img:
-                    m[row][column] = m[row][column] // 4
-            
+        m, n = len(matrix), len(matrix[0])
+        result = [[0] * n for _ in range(m)]
 
-        return 1
+        for i in range(m):
+            for j in range(n):
+                total = 0
+                count = 0
+
+                for r in range(max(0, i - 1), min(m, i + 2)):
+                    for c in range(max(0, j - 1), min(n, j + 2)):
+                        total += matrix[r][c]
+                        count += 1
+
+                result[i][j] = total // count
+
+        return result
     
 
 print(Solution().imageSmoother(img = [[1,1,1],[1,0,1],[1,1,1]]))
