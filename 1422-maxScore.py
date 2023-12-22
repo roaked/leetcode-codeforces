a13e6fe380181b8bcb15511a1b28e096217fc0be
roaked <ossunarola@gmail.com>
2023-12-22 08:25:03 +0100

    maxscore

---
+++ 1422-maxScore.py
@@ -0,0 +1,17 @@
+"""Given a string s of zeros and ones, return the maximum score after splitting the string into two non-empty substrings 
+(i.e. left substring and right substring).
+
+The score after splitting a string is the number of zeros in the left substring plus the number of ones in the right substring."""
+
+
+class Solution:
+    def maxScore(self, s: str) -> int:
+
+        return value
+     
+print(Solution().maxScore(s = "011101"))
+print('\n')
+print(Solution().maxScore(s = "00111"))
+print('\n')
+print(Solution().maxScore(s = "1111"))
+print('\n')
