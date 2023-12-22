585dd03959282f39a090c6cb29e88c2c29b3ff7d
roaked <ossunarola@gmail.com>
2023-12-22 21:39:59 +0000

    there exists a solution if sum(gas) >= sum(cost)

--- 0134-canCompleteCircuit.py
+++ 0134-canCompleteCircuit.py
@@ -9,7 +9,21 @@ Given two integer arrays gas and cost, return the starting gas station's index i
 class Solution:
     def canCompleteCircuit(self, gas: list[int], cost: list[int]) -> int:
         
-        return True
+        total_gas = 0
+        total_cost = 0
+        for idx in range(len(gas)):
+            total_gas += gas[idx]
+            total_cost += cost[idx]
+                
+        if total_gas < total_cost:
+            return -1
+            
+        station, fuel = 0, 0 #station 0
+        for idx in range(len(gas)):
+            fuel += gas[idx] - cost[idx] #condition for moving on
+            if fuel < 0: 
+                fuel, station = 0, idx+1
+        return station
     
 print(Solution().canCompleteCircuit(gas = [1,2,3,4,5], cost = [3,4,5,1,2]))
 print('\n')
