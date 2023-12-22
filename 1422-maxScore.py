10fd4076b41fa8d5ac2f25869eacdb2156b49335
roaked <ossunarola@gmail.com>
2023-12-22 08:47:16 +0100

    modify

--- 1422-maxScore.py
+++ 1422-maxScore.py
@@ -6,8 +6,16 @@ The score after splitting a string is the number of zeros in the left substring 
 
 class Solution:
     def maxScore(self, s: str) -> int:
+        results, ones, zeros = 0,0, s.count('1')
+        for i in range(len(s)-1):
+            if s[i] == '0': 
+                zeros +=1
+            elif s[i] == '1':
+                ones -=1
 
-        return value
+            results = max(results, zeros+ones)
+
+        return results
      
 print(Solution().maxScore(s = "011101"))
 print('\n')
