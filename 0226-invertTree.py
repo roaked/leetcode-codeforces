bf7f99c48a92d0dc589d4aa5c43e777a9bc8cd5b
roaked <ossunarola@gmail.com>
2023-12-23 11:01:43 +0000

    recursion // first invert root and then root.left, root.right

--- 0226-invertTree.py
+++ 0226-invertTree.py
@@ -1,5 +1,7 @@
 """Given the root of a binary tree, invert the tree, and return its root."""
 
+from operations import printTree
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -9,15 +11,26 @@ class TreeNode:
 
 class Solution:
     def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
         
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
         return root
     
-root = TreeNode(3)
-root.left = TreeNode(9)
-root.right = TreeNode(20)
-root.right.left = TreeNode(15)
-root.right.right = TreeNode(7)
+root = TreeNode(4)
+root.left = TreeNode(2)
+root.right = TreeNode(7)
+root.left.left = TreeNode(1)
+root.left.right = TreeNode(3)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(9)
 
 sol = Solution()
-res = (sol.minDepth(root))
-print(res)
+res = (sol.invertTree(root))
+printTree(res)
