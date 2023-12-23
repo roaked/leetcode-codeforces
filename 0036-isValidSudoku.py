2dcc72e8638b05b3991161afd2a8008120667d84
roaked <ossunarola@gmail.com>
2023-12-23 15:05:38 +0000

    nums=[board[r+i][c+j] for i in range(3) for j in range(3)]

--- 0036-isValidSudoku.py
+++ 0036-isValidSudoku.py
@@ -1,34 +1,34 @@
 """Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
 
-Each row must contain the digits 1-9 without repetition.
-Each column must contain the digits 1-9 without repetition.
-Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+C1 - Each row must contain the digits 1-9 without repetition.
+C2 - Each column must contain the digits 1-9 without repetition.
+C3 - Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
 Note:
 
 A Sudoku board (partially filled) could be valid but is not necessarily solvable.
 Only the filled cells need to be validated according to the mentioned rules."""
 
 class Solution:
-    
+
     def isValidSudoku(self, board: list[list[str]]) -> bool:
 
         def valid(arr):
             s=''.join(arr).replace('.','')
             return len(s) == len(set(s))
         
-        def checkrow():
+        def checkrow(): #C1
             for row in board:
                 if not valid(row):
                     return False
             return True
         
-        def checkcol():
+        def checkcol(): #C2
             for col in zip(*board):
                 if not valid(col):
                     return False
             return True
         
-        def square():
+        def square(): #C3
             for r in range(0,9,3):
                 for c in range(0,9,3):
                     nums=[board[r+i][c+j] for i in range(3) for j in range(3)]
