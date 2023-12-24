b0884b25f45a9db278e1d79fb686ef6bd39643cb
roaked <ossunarola@gmail.com>
2023-12-24 19:26:25 +0000

    simple

---
+++ 1732-largestAltitude.py
@@ -0,0 +1,19 @@
+"""There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. 
+The biker starts his trip on point 0 with altitude equal 0.
+
+You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n).
+Return the highest altitude of a point."""
+
+
+class Solution:
+    def largestAltitude(self, gain: list[int]) -> int:
+        high, cum_sum = 0, 0
+        for i in range(len(gain)):
+            cum_sum += gain[i]
+            high = max(high, cum_sum)
+        return high
+        
+
+print(Solution().largestAltitude(gain = [-5,1,5,0,-7]))
+
+print(Solution().largestAltitude(gain = [-4,-3,-2,-1,4,3,2])) 
