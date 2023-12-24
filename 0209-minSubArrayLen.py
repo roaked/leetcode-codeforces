eb6854cea23fe83f02c76d65296bef33a9e3b515
roaked <ossunarola@gmail.com>
2023-12-24 12:22:20 +0000

    recursive dfs to find the smallest combination possible

---
+++ 0209-minSubArrayLen.py
@@ -0,0 +1,43 @@
+"""Given an array of positive integers nums and a positive integer target, return the minimal length of a 
+subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead."""
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: list[int]) -> int:
+        if target in nums:
+            return 1
+
+        if not nums: 
+            return 0
+        
+        left, curr_sum, ans = 0, 0, float('inf')
+        
+        def dfs(start, curr_sum):
+            if curr_sum >= target:
+                ans = min(ans, start - left)    
+                return
+            if start == len(nums):
+                return
+            
+            dfs(start + 1, curr_sum + nums[start])
+        
+        for right in range(len(nums)):
+            curr_sum += nums[right]
+            
+            while curr_sum >= target:
+                ans = min(ans, right - left + 1)
+                curr_sum -= nums[left]
+                left += 1
+        
+        if ans != float('inf'):
+            return ans
+        
+        dfs(0, 0)
+        return ans if ans != float('inf') else 0
+
+        
+    
+print(Solution().minSubArrayLen(target = 7, nums = [2,3,1,2,4,3]))
+print('\n')
+print(Solution().minSubArrayLen(target = 4, nums = [1,4,4]))
+print('\n')
+print(Solution().minSubArrayLen(target = 11, nums = [1,1,1,1,1,1,1,1]))
