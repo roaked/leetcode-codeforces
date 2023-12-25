cad1d8f0125f9d297d49cd5cf08ed3500e213198
roaked <ossunarola@gmail.com>
2023-12-25 23:14:33 +0000

    recursive backtracking for sols in empty cells // difficult

--- 0037-solveSudoku.py
+++ 0037-solveSudoku.py
@@ -9,42 +9,47 @@ Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of
 The '.' character indicates empty cells.
 """
 
-from itertools import product
-from 0036-isValidSudoku import isValidSudoku
-
+from operations import isValidSudoku
 
 class Solution:
 
-    def emptyLocation(self, board: list[list[int]]):
+    def emptyLocation(self, board):
         for i in range(9):
             for j in range(9):
-                if board[i][j] == 0:
+                if board[i][j] == '.':
                     return i, j
         return -1, -1
 
-    def solveSudoku(self, board: list[list[int]]):
-        row, col = emptyLocation(board)
+    def solveSudoku(self, board):
+        row, col = self.emptyLocation(board)
         
         if row == -1 and col == -1:
             return True
         
         for num in range(1, 10):
-            if isValidSudoku(board):
-                board[row][col] = num
-                if solveSudoku(board):
-                    return True
-                board[row][col] = 0
+            next_num = str(num)
+            board[row][col] = next_num
+            if isValidSudoku(self, board) and self.solveSudoku(board):
+                return True
+            board[row][col] = '.'
         
         return False
+    
 
-board = [["5","3",".",".","7",".",".",".","."],
-         ["6",".",".","1","9","5",".",".","."],
-         [".","9","8",".",".",".",".","6","."],
-         ["8",".",".",".","6",".",".",".","3"],
-         ["4",".",".","8",".","3",".",".","1"],
-         ["7",".",".",".","2",".",".",".","6"],
-         [".","6",".",".",".",".","2","8","."],
-         [".",".",".","4","1","9",".",".","5"],
-         [".",".",".",".","8",".",".","7","9"]]
+################################################################
 
-print(Solution().solveSudoku(board))
+board = [
+    ["5","3",".",".","7",".",".",".","."],
+    ["6",".",".","1","9","5",".",".","."],
+    [".","9","8",".",".",".",".","6","."],
+    ["8",".",".",".","6",".",".",".","3"],
+    ["4",".",".","8",".","3",".",".","1"],
+    ["7",".",".",".","2",".",".",".","6"],
+    [".","6",".",".",".",".","2","8","."],
+    [".",".",".","4","1","9",".",".","5"],
+    [".",".",".",".","8",".",".","7","9"]
+]
+
+Solution().solveSudoku(board)
+for row in board:
+    print(row)
