669931153b215ecc25e6bc3768600de8cbd27449
roaked <ossunarola@gmail.com>
2023-12-25 20:32:25 +0000

    simple

--- 2535-differenceOfSum.py
+++ 2535-differenceOfSum.py
@@ -1,5 +1,10 @@
+"""You are given a positive integer array nums.
 
-import math
+The element sum is the sum of all the elements in nums.
+The digit sum is the sum of all the digits (not necessarily distinct) that appear in nums.
+Return the absolute difference between the element sum and digit sum of nums.
+
+Note that the absolute difference between two integers x and y is defined as |x - y|."""
 
 class Solution:
     def differenceOfSum(self, nums: list[int]) -> int:
