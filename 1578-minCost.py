6eb5272e0e0395a9fc5068eca7523043db717b08
roaked <ossunarola@gmail.com>
2023-12-27 19:27:53 +0000

    m

---
+++ 1578-minCost.py
@@ -0,0 +1,19 @@
+"""Alice has n balloons arranged on a rope. You are given a 0-indexed string colors where colors[i] is the color of the ith balloon.
+
+Alice wants the rope to be colorful. She does not want two consecutive balloons to be of the same color, so she asks Bob for help. Bob can remove some balloons from the rope to make it colorful. You are given a 0-indexed integer array neededTime where neededTime[i] is the time (in seconds) that Bob needs to remove the ith balloon from the rope.
+
+Return the minimum time Bob needs to make the rope colorful."""
+
+from typing import List
+
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        time = 0
+        n = len(colors)
+        for i in range(1, n):
+            if colors[i] == colors[i - 1]:
+                time += min(neededTime[i], neededTime[i - 1])
+                neededTime[i] = max(neededTime[i], neededTime[i - 1])
+        return time
+    
+print(Solution().minCost(colors = "abaac", neededTime = [1,2,3,4,5]))
