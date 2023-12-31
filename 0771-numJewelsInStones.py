21f778f980ef159cc9f6d57869fb3f04cab77737
roaked <ossunarola@gmail.com>
2023-12-31 16:08:40 +0000

    hash map

---
+++ 0771-numJewelsInStones.py
@@ -0,0 +1,14 @@
+"""You're given strings jewels representing the types of stones that are jewels, and stones
+ representing the stones you have. Each character in stones is a type of stone you have. 
+ You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A"."""
+from collections import defaultdict, Counter
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        ans, cnt = Counter(stones), 0
+        for key in jewels:
+            cnt += ans[key]
+        return cnt
+
+print(Solution().numJewelsInStones(jewels = "aA", stones = "aAAbbbb"))
