7391e24bf1f6dcf289e11f7670fb2cf28b3e2976
roaked <ossunarola@gmail.com>
2023-12-31 16:32:16 +0000

    skip this one

---
+++ 0709-toLowerCase.py
@@ -0,0 +1,7 @@
+"""Given a string s, return the string after replacing every uppercase letter with the same lowercase letter."""
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
+    
+    
