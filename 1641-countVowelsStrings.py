26481176e0647deefeb3549e3a22103387107b9e
roaked <ossunarola@gmail.com>
2024-01-03 15:51:47 +0100

    using dynamic programming ON time

--- 1641-countVowelsStrings.py
+++ 1641-countVowelsStrings.py
@@ -7,10 +7,11 @@ class Solution:
     def countVowelStrings(self, n: int) -> int:
         
         dp = [[0] * 5 for _ in range(n + 1)]
-        
-        for i in range(5):
+
+        for i in range(5): #base case
             dp[1][i] = 1
-        
+        print(dp)
+
         for i in range(2, n + 1):
             for j in range(5):
                 dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
