e7ee25503bbd9a8e33950440e5e557941c29d7db
roaked <ossunarola@gmail.com>
2024-01-06 14:34:27 +0100

    dp bottom up tabulation, not efficient, redo

--- 0085-maximalRectangle.py
+++ 0085-maximalRectangle.py
@@ -5,21 +5,40 @@ class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
         n, m = len(matrix), len(matrix[0]) # rows, columns
         dp, ans = [[0] * (m) for _ in range(n)], 0
+
         for row in range(n):
-            for col in range(m):
-                if matrix[row][col] != "0": # not null
-                    if row == 0 or col == 0:
-                        dp[row][col] = 1
-                    else:
-                        dp[row][col] = min(dp[row-1][col], dp[row][col-1], dp[row-1][col-1]) + 1
+            dp[row][0] = 1 if matrix[row][0] == '1' else 0
 
-                    ans = max(ans, dp[row][col])  
-        for row in matrix:
-            print(row)
-        for row in dp:
-            print(row)
+        for row in range(n):
+            for col in range(1, m):
+                if matrix[row][col] == '1':
+                    dp[row][col] = dp[row][col - 1] + 1 if dp[row][col - 1] else 1
 
-        return ans * ans
+        for j in range(m):
+            column = []
+            for i in range(n):
+                column.append(dp[i][j])
+            
+            for p in range(len(column)):
+                left = right = p
+                val = column[p]
+                cnt = 1
+
+                while left >= 0 and column[left] >= val:
+                    if left != p:
+                        cnt += 1
+                    left -= 1
+                
+                while right < len(column) and column[right] >= val:
+                    if right != p:
+                        cnt += 1
+                    right += 1
+
+                ans = max(ans, val * cnt)  
+        # print(column, '\n')
+        # for row in dp:
+        #     print(row)
+        return ans
 
 
 print(Solution().maximalRectangle(matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]])) # 6
