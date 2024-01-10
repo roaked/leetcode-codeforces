d6bf46760e11459975f67363db5f2d132529c310
roaked <ossunarola@gmail.com>
2024-01-10 13:25:22 +0100

    one liner

---
+++ 2413-smallestEvenMultiple.py
@@ -0,0 +1,9 @@
+"""Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n."""
+
+
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        return n if n % 2 == 0 else n * 2
+    
+
+print(Solution().smallestEvenMultiple(n = 6))
