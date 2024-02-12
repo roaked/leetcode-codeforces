cd3f55657c5b7267567681d5b6cdf4e45bd7851b
roaked <ossunarola@gmail.com>
2024-02-12 07:25:49 +0100

    easy hash map

---
+++ 0169-majorityElement.py
@@ -0,0 +1,18 @@
+"""Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. 
+You may assume that the majority element always exists in the array.
+"""
+
+from collections import Counter
+from typing import List
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        d, l = Counter(nums), len(nums) // 2
+        for key, value in d.items():
+            if value > l:
+                return key
+        return None
+
+print(Solution().majorityElement(nums = [2,2,1,1,1,2,2]))
+
