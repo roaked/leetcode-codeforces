381bd81f846559fe29adebfa0502684bd050e366
roaked <ossunarola@gmail.com>
2024-02-19 07:29:39 +0100

    mathematics

--- 0231-isPowerOfTwo.py
+++ 0231-isPowerOfTwo.py
@@ -6,8 +6,7 @@ class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         import math
         if not n > 0: return False
-        x = int(math.log(n, 2))
-        return pow(2, x) == n
+        return pow(2, int(math.log(n, 2))) == n
 
 
 print(Solution().isPowerOfTwo(n = 3))
